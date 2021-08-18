cb88a44c8d19fda5ce1543bcdf0e2d015cd2da51
Nat <ntaktach@gmail.com>
2021-08-18 17:45:39 -0400

    Done.

---
+++ src/components/MovieShow.js
@@ -0,0 +1,12 @@
+// ./src/components/MovieShow.js
+import React from "react";
+
+const MovieShow = (props) => {
+  return (
+    <div>
+      <h3>Movies Show Component!</h3>
+    </div>
+  );
+};
+
+export default MovieShow;
